ace534ea3dee37075f7ad7283b732969aae745fb
Bernhard Liebl <Bernhard.Liebl@rz.uni-regensburg.de>
2018-08-02 10:38:41 +0200

    support pdb-clone

--- headless/robot/pkg/__main__.py
+++ headless/robot/pkg/__main__.py
@@ -7,6 +7,14 @@
 
 import sys
 
+try:
+	# if pdb-clone is installed, register this process. in
+	# case of problems, we can attach and debug.
+	from pdb_clone import pdbhandler
+	pdbhandler.register()
+except ImportError:
+	pass
+
 if sys.argv[1] == "master":
 	from .http.master import run_master
 	run_master()
