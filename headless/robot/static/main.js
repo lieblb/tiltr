13715b34fe4bd79ce983395f34df961883e90e7a
Bernhard Liebl <Bernhard.Liebl@rz.uni-regensburg.de>
2018-10-25 13:51:00 +0200

    Better log display: delay scroll, add colored labels

--- headless/robot/static/main.js
+++ headless/robot/static/main.js
@@ -10,6 +10,34 @@ $(function() {
 	var workaroundKeys = [];
 	var settingKeys = [];
 
+	// https://gist.github.com/mjackson/5311256
+	function hslToRgb(h, s, l) {
+	  var r, g, b;
+
+	  if (s == 0) {
+		r = g = b = l; // achromatic
+	  } else {
+		function hue2rgb(p, q, t) {
+		  if (t < 0) t += 1;
+		  if (t > 1) t -= 1;
+		  if (t < 1/6) return p + (q - p) * 6 * t;
+		  if (t < 1/2) return q;
+		  if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+		  return p;
+		}
+
+		var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		var p = 2 * l - q;
+
+		r = hue2rgb(p, q, h + 1/3);
+		g = hue2rgb(p, q, h);
+		b = hue2rgb(p, q, h - 1/3);
+	  }
+
+	  return `rgb(${r * 255}, ${g * 255}, ${b * 255})`;
+	  //return [ r * 255, g * 255, b * 255 ];
+	}
+
 	$.getJSON(host + "/preferences.json", function(preferences) {
 		var settings = preferences.settings;
 		for (var i = 0; i < settings.length; i++) {
@@ -397,6 +425,16 @@ $(function() {
 
 	function report(machine, message) {
 		var tag = $('<span class="tag is-light"></span>');
+
+		var machineNo = machine.match(/\d+/g);
+		if (machineNo == null) {
+			machineNo = 0;
+		} else {
+			machineNo = Number(machineNo[0]);
+		}
+		tag.css("background-color", hslToRgb(machineNo / 20, 0.5, 0.75));
+
+		tag.css("color", "black");
 		tag.text(machine);
 
 		var entry = $("<div></div>");
@@ -407,8 +445,12 @@ $(function() {
 		entry.append(text);
 
 		var log = $("#log");
+		var updateScroll = (
+			log.scrollTop() >= log[0].scrollHeight - log.height() - 50);
 		log.append(entry);
-		log.scrollTop(log[0].scrollHeight - log.height());
+		if (updateScroll) {
+			log.scrollTop(log[0].scrollHeight - log.height());
+		}
 
 		if (!screenshots.dirty[machine]) {
 			screenshots.dirty[machine] = true;
