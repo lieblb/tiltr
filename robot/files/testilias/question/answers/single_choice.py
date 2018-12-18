4c4f8bb65e7af43fa1dba1b0acc6f43899298f88
Bernhard Liebl <Bernhard.Liebl@rz.uni-regensburg.de>
2018-12-18 11:14:38 +0100

    Added additional protocols for single choice.

--- robot/files/testilias/question/answers/single_choice.py
+++ robot/files/testilias/question/answers/single_choice.py
@@ -22,41 +22,41 @@ class SingleChoiceAnswer(Answer):
 		return Validness.VALID
 
 	def _set_answer(self, answer, score):
+		answer_found = False
 		for choice in self._parse_ui():
 			self.protocol.choose(choice.label, choice.label == answer)
 			if choice.label == answer:
 				self.driver.find_element_by_css_selector(choice.selector).click()
-		self.current_answer = answer
-		self.current_score = score
+				self.current_answer = answer
+				self.current_score = score
+				self.protocol.add("on _set_answer: current_answer == '%s'" % answer)
+				answer_found = True
+				break
+		if not answer_found:
+			raise Exception("could not find answer '%s'" % answer)
 
 	def _parse_ui(self):
 		choices = []
 		for answer in self.driver.find_elements_by_css_selector('.ilc_question_SingleChoice .ilc_qanswer_Answer'):
 			radio = answer.find_element_by_css_selector('input[type="radio"]')
 			label = answer.find_element_by_css_selector('label[for="%s"]' % radio.get_attribute("id"))
-			c = Choice(
+			choices.append(Choice(
 				selector="#" + radio.get_attribute("id"),
 				label=label.text.strip(),
-				checked=radio.is_selected())
-			choices.append(c)
+				checked=radio.is_selected()))
 		return choices
 
 	def verify(self, context, after_crash=False):
+		self.protocol.add("on verify: current_answer == '%s'" % self.current_answer)
 		for c in self._parse_ui():
-			if c.label == self.current_answer:
-				expected = True
-			else:
-				expected = False
+			expected = (c.label == self.current_answer)
 			self.protocol.verify(c.label, expected, c.checked, after_crash=after_crash)
 		context.coverage.case_occurred(self.question, "verify", self.current_answer)
 
 	def to_dict(self, context, language):
-		answers = dict()
-		for choice in self.question.choices.keys():
-			if choice == self.current_answer:
-				answers[choice] = 1
-			else:
-				answers[choice] = 0
+		answers = dict(
+			(choice, 1 if choice == self.current_answer else 0)
+			for choice in self.question.choices.keys())
 		return dict(
 			title=self.question.title,
 			answers=answers,
