ac382fcd3877fd8e1f1a410d25d61480b375c0f9
Bernhard Liebl <Bernhard.Liebl@rz.uni-regensburg.de>
2019-04-12 09:35:45 +0200

    explicitly enable readjustment settings

--- docker/db/changes.sql
+++ docker/db/changes.sql
@@ -3,4 +3,9 @@
 UPDATE usr_data SET passwd="$2y$10$/YK5.2Sn.QhxrTfrSuggJO3QkvpgO77aVisnbTL7tbtL2JlUg1sgK" WHERE login="root";
 UPDATE usr_data SET last_password_change=NOW() WHERE login="root";
 UPDATE usr_pref SET value="de" WHERE keyword="language";
-UPDATE settings SET value=0 WHERE keyword='ps_password_change_on_first_login_enabled';
+
+-- patch settings
+UPDATE settings SET value=0 WHERE module="common" AND keyword="ps_password_change_on_first_login_enabled";
+
+UPDATE settings SET value='13,5,10,14,12,7,15,8,6,17,3,2,9,1,4,18' WHERE module="assessment" AND keyword="assessment_scoring_adjustment";
+UPDATE settings SET value='1' WHERE module="assessment" AND keyword="assessment_adjustments_enabled";
